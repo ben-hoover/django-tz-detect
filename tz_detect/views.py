91450a2aaef608106a6265b4a53d12372db030d5
ben-hoover <bhoover@seas.upenn.edu>
2020-07-01 11:54:36 -0700

    added new tz method

--- tz_detect/views.py
+++ tz_detect/views.py
@@ -8,15 +8,16 @@ class SetOffsetView(View):
     http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
-        offset = request.POST.get('offset', None)
-        if not offset:
-            return HttpResponse("No 'offset' parameter provided", status=400)
-
-        try:
-            offset = int(offset)
-        except ValueError:
-            return HttpResponse("Invalid 'offset' value provided", status=400)
-
-        request.session['detected_tz'] = int(offset)
-
+        tz = request.POST.get('tz', None)
+        if not tz:
+            offset = request.POST.get('offset', None)
+            if not offset:
+                return HttpResponse("No 'offset' parameter provided", status=400)
+            try:
+                offset = int(offset)
+            except ValueError:
+                return HttpResponse("Invalid 'offset' value provided", status=400)
+            request.session['detected_tz'] = int(offset)
+        else:
+            request.session['detected_tz'] = tz
         return HttpResponse("OK")
